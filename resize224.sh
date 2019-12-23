eb236914e723d9a7a405e6716875e0918c72f679
zhengmeisong <zhengmeisong@hpcgpu39.ai.zzzc.qihoo.net>
2019-12-23 17:04:32 +0800

    resize images

--- resize224.sh
+++ resize224.sh
@@ -1,3 +1,3 @@
-python resize.py --saveRoot ./224/ --rw 224 --rH 224 \
+python resize.py --saveRoot ./224/ --rW 224 --rH 224 \
                  --inRoot /ai_data/zhengmeisong/defake/data/faces_detection/ \
                  --listfile list.txt
